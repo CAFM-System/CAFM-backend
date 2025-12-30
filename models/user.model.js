10701c6e424c8d62be8cb950f403ae4fd1cd0577
Danujainoshika <danujamanukularathna@gmail.com>
2025-12-30 23:40:09 +0530

    Enhance technician retrieval by adding job type filtering in API

--- models/user.model.js
+++ models/user.model.js
@@ -1,15 +1,25 @@
 import { supabase, supabaseAdmin } from "../config/supabaseClient.js";
 
-const getTechnicians = async () => {
+const getTechnicians = async (jobType) => {
     const { data, error } = await supabase
         .from('profiles')
-        .select("user_id,first_name,last_name")
-        .eq('role', 'technician');
+        .select(
+            `user_id,
+            first_name,
+            last_name,
+            technicians!inner(
+                job
+            )
+            `)
+        .eq('role', 'technician')
+        .eq('technicians.job', jobType);
         
 
     if (error) {
         throw new Error('Error fetching technicians: ' + error.message);
     }
+    
+
     return data;
 }
 
