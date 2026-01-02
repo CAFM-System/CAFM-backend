7ac41949ec707cbb73b664787cdbf23195960815
DMDI Manukularathna <danujamanukularathna@gmail.com>
2026-01-02 13:21:20 +0530

    Enhance technician retrieval by adding job type filtering in API (#20)

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,10 +2,18 @@ import { getTechnicians } from "../models/user.model.js";
 
 const getAllTechnicians = async (req, res) => {
     try {
+        const {jobType} = req.query;
+        console.log(jobType);
+        if(!jobType){
+            return res.status(400).json({ message: "Job type is required" });
+        }
         
-        const technicians = await getTechnicians();
+
+        const technicians = await getTechnicians(jobType);
         res.status(200).json(technicians);
-        console.log("Fetched Technicians:", technicians);
+        console.log(technicians);
+          
+        
          
     } catch (error) {
         res.status(500).json({ message: "Failed to fetch technicians", error: error.message });
