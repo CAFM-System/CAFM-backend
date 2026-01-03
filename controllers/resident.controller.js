d2d2b90ec2d683d89d5335e88082a4be14dad409
Danujainoshika <danujamanukularathna@gmail.com>
2026-01-03 13:33:01 +0530

    Add ticket reopening functionality with email notifications

--- controllers/resident.controller.js
+++ controllers/resident.controller.js
@@ -1,7 +1,7 @@
 import { notifyAdmins, notifyUserById } from "../models/notification.model.js";
 import { addProgressHistoryEntry } from "../models/progressHistory.model.js";
 import { getTicketById, updateTicket } from "../models/ticket.Model.js";
-import { closedTicketEmail } from "../utils/emailTemplates.js";
+import { closedTicketEmail, reOpenTicketEmail } from "../utils/emailTemplates.js";
 
 const closeTicket = async (req , res)=>{
     try {
@@ -43,4 +43,39 @@ const closeTicket = async (req , res)=>{
     }
 }
 
-export { closeTicket };
+const reOpenTicket = async (req , res)=>{
+    try {
+        const { ticketId } = req.params;
+        const message = req.body.message || "Ticket reopened by resident";
+        const ticket = await getTicketById(ticketId);
+        if(!ticket){
+            return res.status(404).json({ message: "Ticket not found" });
+        }
+        const newticket = await updateTicket(ticketId, { status: "open" });
+
+        await notifyAdmins(
+            "Ticket Reopened",
+            reOpenTicketEmail(newticket)
+        )
+
+        await addProgressHistoryEntry(
+            {
+                ticket_id: ticketId,
+                status: "open",
+                updated_by: `Resident (${req.user.name})`,
+                message: message
+            }
+        )
+
+        res.status(200).json({
+            message: "Ticket reopened successfully",
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Failed to reopen ticket", error: error.message });
+    }
+}
+
+export { closeTicket , reOpenTicket};
+
+
+
--- routes/api/resident.routes.js
+++ routes/api/resident.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { closeTicket } from "../../controllers/resident.controller.js";
+import { closeTicket, reOpenTicket } from "../../controllers/resident.controller.js";
 import authenticate from "../../middlewares/auth.js";
 import checkRole from "../../middlewares/roleCheck.js";
 
 const residentRouter = express.Router();
 
 residentRouter.put("/close-ticket/:ticketId",authenticate,checkRole("resident"),closeTicket);
+residentRouter.put("/reopen-ticket/:ticketId",authenticate,checkRole("resident"),reOpenTicket);
 
 export default residentRouter;
