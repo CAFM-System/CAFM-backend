55302b3751cdfdedf6815a820b4a1f463503042a
Danujainoshika <danujamanukularathna@gmail.com>
2026-01-28 10:45:59 +0530

    feat: enhance visitor pre-registration and QR code scanning functionality

--- routes/api/visitor.routes.js
+++ routes/api/visitor.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { preRegisterVisitor } from "../../controllers/visitor.controller.js";
+import { preRegisterVisitor, scanVisitorQr } from "../../controllers/visitor.controller.js";
 import authenticate from "../../middlewares/auth.js";
 import checkRole from "../../middlewares/roleCheck.js";
 
 const visitorRouter = express.Router();
 
-visitorRouter.post("/",authenticate,checkRole("resident"),preRegisterVisitor);
+visitorRouter.post("/pre-register",authenticate,checkRole("resident"),preRegisterVisitor);
+visitorRouter.post("/scan",authenticate,scanVisitorQr);
 
 export default visitorRouter;
