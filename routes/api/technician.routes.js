b0e47f71433c5175139e3eadb743f95ed5f19136
Danujainoshika <danujamanukularathna@gmail.com>
2026-03-16 00:42:27 +0530

    feat: add fetchAttachments endpoint to retrieve ticket attachments

--- routes/api/technician.routes.js
+++ routes/api/technician.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import authenticate from '../../middlewares/auth.js';
 import checkRole from '../../middlewares/roleCheck.js';
-import { addWorkStartTime, resolveTicket } from '../../controllers/technician.controller.js';
+import { addWorkStartTime, fetchAttachments, resolveTicket } from '../../controllers/technician.controller.js';
 
 const technicianRouter = express.Router();
 
 
 technicianRouter.put("/update-time/:ticketId",authenticate,checkRole("technician"), addWorkStartTime);
 technicianRouter.put("/resolve-ticket/:ticketId",authenticate,checkRole("technician"), resolveTicket);
+technicianRouter.get("/attachments/:ticketId", authenticate, checkRole("technician"), fetchAttachments);
 
 export default technicianRouter;
